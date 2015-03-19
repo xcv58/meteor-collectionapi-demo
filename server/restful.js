34132bd2e3d200b8f34dc6a8f4e287f97dcf6647
xcv58 <chenyihonglove@gmail.com>
2015-03-19 19:22:05 -0400

    add v0.2.1 example, don't really delete data but set _del to true

--- server/restful.js
+++ server/restful.js
@@ -25,7 +25,7 @@ if (Meteor.isServer) {
         console.log("method: " + method);
         console.log("requestMetadata: " + JSON.stringify(requestMetadata));
         if (token === undefined) {
-          return false;
+          return true;
         }
         if (token === "97f0ad9e24ca5e0408a269748d7fe0a0") {
           return false;
@@ -35,10 +35,114 @@ if (Meteor.isServer) {
       methods: ['POST','GET','PUT','DELETE'],  // Allow creating, reading, updating, and deleting
       before: {  // This methods, if defined, will be called before the POST/GET/PUT/DELETE actions are performed on the collection.
                  // If the function returns false the action will be canceled, if you return true the action will take place.
-        POST: undefined,    // function(obj, requestMetadata) {return true/false;},
-        GET: undefined,     // function(objs, requestMetadata) {return true/false;},
-        PUT: undefined,     // function(obj, newValues, requestMetadata) {return true/false;},
-        DELETE: undefined  // function(obj, requestMetadata) {return true/false;}
+        // POST: undefined,    // function(obj, requestMetadata, returnObject) {return true/false;},
+        POST: function(obj, requestMetadata, returnObject) {
+          // always set returnObject.success = true, if you want handle it by yourself!
+          returnObject.success = true;
+
+          // only allow obj to insert with 'id' key exists.
+          var hasId = obj.hasOwnProperty('id');
+          if (hasId) {
+            try {
+              var id = Players.insert(obj);
+              returnObject.statusCode = 201;
+              obj._id = id;
+              returnObject.body = {
+                method: 'POST',
+                obj: obj
+              };
+            } catch (e) {
+              returnObject.statusCode = 500;
+              returnObject.body = {
+                error: e.toString()
+              };
+            }
+          } else {
+            returnObject.statusCode = 500;
+            returnObject.body = {error: 'no id'};
+          }
+          return true;
+        },
+        // GET: undefined,     // function(objs, requestMetadata, returnObject) {return true/false;},
+        GET: function (objs, requestMetadata, returnObject) {
+          returnObject.success = true;
+
+          // only expose obj with no _del or _del === false
+          // You may need manually get objs if you pass in an invalid collection
+          returnObject.statusCode = 200;
+          var filteredObjs = [];
+          objs.forEach(function(obj) {
+            if (!obj.hasOwnProperty("_del") || obj._del === false) {
+              filteredObjs.push(obj);
+            }
+          });
+          returnObject.body = {
+            method: 'GET',
+            objs: filteredObjs
+          };
+          return true;
+        },
+        // PUT: undefined,     // function(obj, newValues, requestMetadata, returnObject) {return true/false;},
+        PUT: function(obj, newValues, requestMetadata, returnObject) {
+          returnObject.success = true;
+
+          // if (!obj || obj._del === true) {
+          // even _del equals true, user still can set it to false to activate it.
+          if (!obj) {
+            returnObject.statusCode = 500;
+            returnObject.body = {
+              error: "id " + requestMetadata.collectionId + " doesn't exist!"
+            };
+            return true;
+          }
+
+          try {
+            var updatedObj = Players.update(obj._id, newValues);
+            returnObject.statusCode = 200;
+            returnObject.body = {
+              method: 'PUT',
+              obj: updatedObj
+            };
+          } catch (e) {
+              returnObject.statusCode = 500;
+              returnObject.body = {
+                error: e.toString()
+              };
+          }
+
+          return true;
+        },
+        // DELETE: undefined  // function(obj, requestMetadata, returnObject) {return true/false;}
+        DELETE: function(obj, requestMetadata, returnObject) {
+          returnObject.success = true;
+
+          if (!obj || obj._del === true) {
+            returnObject.statusCode = 500;
+            returnObject.body = {
+              error: "id " + requestMetadata.collectionId + " doesn't exist!"
+            };
+            return true;
+          }
+
+          try {
+            Players.update(obj._id, {
+              "$set": {
+                "_del": true
+              }
+            });
+            returnObject.statusCode = 200;
+            returnObject.body = {
+              method: 'DELETE'
+            };
+          } catch (e) {
+              returnObject.statusCode = 500;
+              returnObject.body = {
+                error: e.toString()
+              };
+          }
+
+          return true;
+        }
       },
       after: {  // This methods, if defined, will be called after the POST/GET/PUT/DELETE actions are performed on the collection.
                 // Generally, you don't need this, unless you have global variable to reflect data inside collection.
